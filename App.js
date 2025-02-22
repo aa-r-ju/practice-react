65371a1d2d7411cd8d8ee28873361c620955ca8b
aa-r-ju <aarju3447@gmail.com>
2025-02-22 21:45:49 +0545

    added siblings also in child

--- App.js
+++ App.js
@@ -4,15 +4,16 @@
 
 // root.render(heading);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", { id: "h1" }, "I am h1 tag")
-  )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "h1" }, "I am h1 tag"),
+    React.createElement("h1", { id: "h2" }, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "h1" }, "I am h1 tag"),
+    React.createElement("h1", { id: "h2" }, "I am h2 tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
