027f6f643a1bdad2c69f6572059895b31916fc0b
aa-r-ju <aarju3447@gmail.com>
2025-02-22 21:34:17 +0545

    added nested structure in App.js

--- App.js
+++ App.js
@@ -3,12 +3,27 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "h1" }, "I am h1 tag")
+  )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
